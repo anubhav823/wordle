9a6f485451f3a54765556e63ed24eb13eff78625
Anubhav Jain <anubhav823@gmail.com>
2022-05-07 16:20:52 +0530

    method refactoring

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ let line4 = document.getElementById('line4')
 let line5 = document.getElementById('line5')
 let line6 = document.getElementById('line6')
 
-const word = 'TREVOR'
+const word = 'TREVOR';
 
-// console.log(RandomWord());
 function moveOnMax(field, nextFieldID) {
     console.log("field "+field);
     // console.log("maxLength"+field.maxLength;
@@ -15,6 +14,15 @@ function moveOnMax(field, nextFieldID) {
         console.log("This "+nextFieldID)
         nextFieldID.focus(); 
     } 
+}
+
+function disable(id){
+    console.log(id);
+    document.getElementById(id).getElementsByClassName('input_box').disabled = true
+}
+
+function enable(){
+
 }
 
 function checkIfCharacterIsPresent(s){
@@ -25,10 +33,12 @@ function checkIfCharacterIsPresent(s){
     }
     return false;
 }
-line1.addEventListener("keyup", function (event) {
+
+function addEventListenerToLine(event){
     let flag = 1;
-    const elem = document.querySelector("#line1")
-    let array = [...elem.childNodes];
+    console.log(event.currentTarget);
+    let line = event.currentTarget;
+    let array = [...line.childNodes];
     if(event.code !== 'Enter'){
         for(let i = 0;i< array.length;i++){
             if(i!== array.length-1){
@@ -59,183 +69,13 @@ line1.addEventListener("keyup", function (event) {
             alert('Done! You got the word')
         }
     }
-});
-
-line2.addEventListener("keyup", function (event) {
-    let flag = 1;
-    const elem = document.querySelector("#line2")
-    let array = [...elem.childNodes];
-    if(event.code !== 'Enter'){
-        for(let i = 0;i< array.length;i++){
-            if(i!== array.length-1){
-                if (array[i].value.length >= array[i].maxLength && array[i+1].value.length === 0) {
-                    array[i+1].focus(); 
-                } 
-            }
-        }
-    }
-    if (event.code === 'Enter') {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i].value.length != 1) {
-                console.log(array[i].value + " logs")
-                alert('found empty space line1')
-                break;
-            } else if (array[i].value == word[i]) {
-                array[i].style.backgroundColor = 'green'
-            } else if( checkIfCharacterIsPresent(array[i].value)){
-                 array[i].style.backgroundColor = 'yellow'
-                 flag = 0;
-            } else {
-                array[i].style.backgroundColor = 'red'
-                flag = 0;
-            }
-        }
-        if(flag == 1){
-            alert('Done! You got the word')
-        }
-    }
-});
-
-line3.addEventListener("keyup", function (event) {
-    let flag = 1;
-    const elem = document.querySelector("#line3")
-    let array = [...elem.childNodes];
-    if(event.code !== 'Enter'){
-        for(let i = 0;i< array.length;i++){
-            if(i!== array.length-1){
-                if (array[i].value.length >= array[i].maxLength && array[i+1].value.length === 0) {
-                    array[i+1].focus(); 
-                } 
-            }
-        }
-    }
-    if (event.code === 'Enter') {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i].value.length != 1) {
-                console.log(array[i].value + " logs")
-                alert('found empty space line1')
-                break;
-            } else if (array[i].value == word[i]) {
-                array[i].style.backgroundColor = 'green'
-            } else if( checkIfCharacterIsPresent(array[i].value)){
-                 array[i].style.backgroundColor = 'yellow'
-                 flag = 0;
-            } else {
-                array[i].style.backgroundColor = 'red'
-                flag = 0;
-            }
-        }
-        if(flag == 1){
-            alert('Done! You got the word')
-        }
-    }
-});
-
-line4.addEventListener("keyup", function (event) {
-    let flag = 1;
-    const elem = document.querySelector("#line4")
-    let array = [...elem.childNodes];
-    if(event.code !== 'Enter'){
-        for(let i = 0;i< array.length;i++){
-            if(i!== array.length-1){
-                if (array[i].value.length >= array[i].maxLength && array[i+1].value.length === 0) {
-                    array[i+1].focus(); 
-                } 
-            }
-        }
-    }
-    if (event.code === 'Enter') {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i].value.length != 1) {
-                console.log(array[i].value + " logs")
-                alert('found empty space line1')
-                break;
-            } else if (array[i].value == word[i]) {
-                array[i].style.backgroundColor = 'green'
-            } else if( checkIfCharacterIsPresent(array[i].value)){
-                 array[i].style.backgroundColor = 'yellow'
-                 flag = 0;
-            } else {
-                array[i].style.backgroundColor = 'red'
-                flag = 0;
-            }
-        }
-        if(flag == 1){
-            alert('Done! You got the word')
-        }
-    }
-});
-
-line5.addEventListener("keyup", function (event) {
-    let flag = 1;
-    const elem = document.querySelector("#line5")
-    let array = [...elem.childNodes];
-    if(event.code !== 'Enter'){
-        for(let i = 0;i< array.length;i++){
-            if(i!== array.length-1){
-                if (array[i].value.length >= array[i].maxLength && array[i+1].value.length === 0) {
-                    array[i+1].focus(); 
-                } 
-            }
-        }
-    }
-    if (event.code === 'Enter') {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i].value.length != 1) {
-                console.log(array[i].value + " logs")
-                alert('found empty space line1')
-                break;
-            } else if (array[i].value == word[i]) {
-                array[i].style.backgroundColor = 'green'
-            } else if( checkIfCharacterIsPresent(array[i].value)){
-                 array[i].style.backgroundColor = 'yellow'
-                 flag = 0;
-            } else {
-                array[i].style.backgroundColor = 'red'
-                flag = 0;
-            }
-        }
-        if(flag == 1){
-            alert('Done! You got the word')
-        }
-    }
-});
-
-line6.addEventListener("keyup", function (event) {
-    let flag = 1;
-    const elem = document.querySelector("#line6")
-    let array = [...elem.childNodes];
-    if(event.code !== 'Enter'){
-        for(let i = 0;i< array.length;i++){
-            if(i!== array.length-1){
-                if (array[i].value.length >= array[i].maxLength && array[i+1].value.length === 0) {
-                    array[i+1].focus(); 
-                } 
-            }
-        }
-    }
-    if (event.code === 'Enter') {
-        for (let i = 0; i < array.length; i++) {
-            if (array[i].value.length != 1) {
-                console.log(array[i].value + " logs")
-                alert('found empty space line1')
-                break;
-            } else if (array[i].value == word[i]) {
-                array[i].style.backgroundColor = 'green'
-            } else if( checkIfCharacterIsPresent(array[i].value)){
-                 array[i].style.backgroundColor = 'yellow'
-                 flag = 0;
-            } else {
-                array[i].style.backgroundColor = 'red'
-                flag = 0;
-            }
-        }
-        if(flag == 1){
-            alert('Done! You got the word')
-        }
-    }
-});
-
+}
+line1.addEventListener("keyup", addEventListenerToLine);
+line2.addEventListener("keyup", addEventListenerToLine);
+line3.addEventListener("keyup", addEventListenerToLine);
+line4.addEventListener("keyup", addEventListenerToLine);
+line5.addEventListener("keyup", addEventListenerToLine);
+line6.addEventListener("keyup", addEventListenerToLine);
 
 
 
@@ -244,9 +84,14 @@ function populateTextBoxes(line) {
         let input = document.createElement('input');
         input.type = "text";
         input.maxLength = 1;
+        input.disabled = false;
         input.classList.add('input_box');
         line.appendChild(input)
     }
+}
+
+function enable(){
+
 }
 populateTextBoxes(line1)
 populateTextBoxes(line2)
