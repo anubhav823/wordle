3a75583ad76cd4690c548b5a13b76ee3e11bce2c
Anubhav Jain <anubhav823@gmail.com>
2022-05-07 16:23:55 +0530

    method refactoring

--- index.js
+++ index.js
@@ -8,43 +8,43 @@ let line6 = document.getElementById('line6')
 const word = 'TREVOR';
 
 function moveOnMax(field, nextFieldID) {
-    console.log("field "+field);
+    console.log("field " + field);
     // console.log("maxLength"+field.maxLength;
     if (field.value.length >= field.maxLength && nextFieldID.value.length === 0) {
-        console.log("This "+nextFieldID)
-        nextFieldID.focus(); 
-    } 
+        console.log("This " + nextFieldID)
+        nextFieldID.focus();
+    }
 }
 
-function disable(id){
+function disable(id) {
     console.log(id);
     document.getElementById(id).getElementsByClassName('input_box').disabled = true
 }
 
-function enable(){
+function enable() {
 
 }
 
-function checkIfCharacterIsPresent(s){
-    for(let i = 0;i<word.length;i++){
-        if(word[i]==s){
+function checkIfCharacterIsPresent(s) {
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] == s) {
             return true;
         }
     }
     return false;
 }
 
-function addEventListenerToLine(event){
+function addEventListenerToLine(event) {
     let flag = 1;
     console.log(event.currentTarget);
     let line = event.currentTarget;
     let array = [...line.childNodes];
-    if(event.code !== 'Enter'){
-        for(let i = 0;i< array.length;i++){
-            if(i!== array.length-1){
-                if (array[i].value.length >= array[i].maxLength && array[i+1].value.length === 0) {
-                    array[i+1].focus(); 
-                } 
+    if (event.code !== 'Enter') {
+        for (let i = 0; i < array.length; i++) {
+            if (i !== array.length - 1) {
+                if (array[i].value.length >= array[i].maxLength && array[i + 1].value.length === 0) {
+                    array[i + 1].focus();
+                }
             }
         }
     }
@@ -57,15 +57,15 @@ function addEventListenerToLine(event){
             }
             if (array[i].value == word[i]) {
                 array[i].style.backgroundColor = 'lightgreen'
-            } else if( checkIfCharacterIsPresent(array[i].value)){
-                 array[i].style.backgroundColor = 'yellow'
-                 flag = 0;
+            } else if (checkIfCharacterIsPresent(array[i].value)) {
+                array[i].style.backgroundColor = 'yellow'
+                flag = 0;
             } else {
                 array[i].style.backgroundColor = 'red'
                 flag = 0;
             }
         }
-        if(flag == 1){
+        if (flag == 1) {
             alert('Done! You got the word')
         }
     }
@@ -90,7 +90,7 @@ function populateTextBoxes(line) {
     }
 }
 
-function enable(){
+function enable() {
 
 }
 populateTextBoxes(line1)
